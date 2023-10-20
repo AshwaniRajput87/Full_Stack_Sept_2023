3ea3b5fdf59c35aa4cc17a5ec11dc561b32da96c
Ashwani Rajput <ashwin.rajput87@gmail.com>
2023-10-20 10:59:23 +0530

    FEMC-4 - Nested comments and Memory leak problems

---
+++ FE-Machine-Coding/4_Nested_Comment_Box_Optimisation_JS_Memory_Leaks_Browser_Perf/Memory_Leak_Problems_&_Avoid_Them.js
@@ -0,0 +1,84 @@
+/***
+ *    Common JS memory leaks and how to avoid them:
+ * 
+ *    1. Global variables: 
+ *   
+ *        -> It may be pollutted (modified via function)
+ * 
+ *        -> Try to avoid using global variables or use under "use strict"
+ * 
+ *        -> use let and const instead of var.
+ * 
+ *     2. closures and scope:
+ *      
+ *        -> use proper scope and closures data clean up part
+ * 
+ *     3. Large objects: array 1000 data at a time you fetching
+ * 
+ *        -> Derefernce the large objects;
+ *           const data = [
+ *            {
+ *              .........
+ *              .......
+ *             }
+ *        ];
+ *        data = null;
+ * 
+ *      4. Use mordern JS practices:
+ *         -> use let and const instead of var.
+ *         -> use arrow function expression
+ *         -> use export and import ( creating modules-> reusable code) -> utils
+ *         -> Use proper scoping -> use Classes (OOPS)and IIFE
+ * 
+ *            const PaymentModule = (()=>{ // FP
+ * 
+ *               
+ *            })();
+ *      5. Manage your timers and intervals
+ *         const timerId = setInterval(()=> {
+ *           // Do something
+ *        }, 1000);
+ * 
+ *        // clear the interval, when it's no longer.
+ *         //clearInterval(timerId);
+ *      6. DOM API leak problem:
+ * 
+ *          const btn = document.getElementById('btn');
+ *          btn.addEventListerner('click' ()=> {this.clickMethod()});
+ * 
+ *          To imporove DOM API leakage prablems:
+ *           -> event.stopPropation(); -> avoid the event bubbling
+ *           -> btn.removeEventListerner('click' ()=> {this.clickMethod()});
+ * 
+ *      7. Avoiding forgotten promises:
+ *         
+ *         const promise = someAsyncFunction(); // forgotten to use or consume the promises;
+ *         new Promise((resolve, reject)=>{
+ *              if(data) {
+ *                  resolve(data);
+ *              } else {
+ *                  reject(error);
+ *             }
+ *          })
+ *         promise.then(()=>{
+ *           // result handling
+ *           // const res = {...data};
+ *               res = null;
+ *         }).catch(()=>{
+ *           // error handling
+ *         });
+ * 
+ */
+
+"use strict";
+
+// var b = undefined;
+let b=20;
+console.log(b);
+
+function fetchindData() {
+
+    const data = fetchData(); // Data will fetching in this method via fetch API.
+
+    data = null; // release the reference
+}
---
+++ FE-Machine-Coding/4_Nested_Comment_Box_Optimisation_JS_Memory_Leaks_Browser_Perf/Mini_JS_Projects/Count-Down-Timer/js/script.js
@@ -0,0 +1,193 @@
+/**
+ *  PS: Implement the Count down timer.
+ * 
+ *  Requirements:
+ *    pass values: in mins and seconds input fields
+ *    play , stop, reset
+ *    validation on each input fields -> no -ve  values are allowed
+ *    
+ *    good to have:
+ *      reset the timer
+ * 
+ *    Approach:   min : sec
+ *     test case: 10 : 00, get the values from each input filed of mins and secs
+ *                minLSB, minMSB, secMSB, secLSB
+ *     3 - Buttons
+ *        - start - to start the timer
+ *        - stop - to stop the timer 
+ *        - reset - to reset the input values as 0
+ */
+
+// declare the variables here -> global variables
+// function -> local variables
+
+"use strict";
+
+class Timer {
+    constructor(minMSB, minLSB, secMSB, secLSB, start, stop, reset, display){
+        this.minMSB = document.getElementById(minMSB);
+        this.minLSB = document.getElementById(minLSB);
+        this.secMSB = document.getElementById(secMSB);
+        this.secLSB = document.getElementById(secLSB);
+
+        this.start = document.getElementById(start);
+        this.stop = document.getElementById(stop);
+        this.reset = document.getElementById(reset);
+
+        this.display = document.getElementById(display);
+
+        this.minutes = 0;
+        this.seconds = 0;
+        this.intervalId = null;
+
+        // this.prevVal = '';
+        // this.currentVal = '';
+
+        // this.onStart = this.onStart.bind(this);
+        // this.start.addEventListener('click', this.onStart);
+        // this.onStop = this.onStop.bind(this);
+        // this.stop.addEventListener('click', this.onStop);
+        // this.onReset = this.onReset.bind(this);
+        // this.reset.addEventListener('click', this.onReset);
+
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        this.start.addEventListener('click', () => {this.onStart()});
+        this.stop.addEventListener('click', () => {this.onStop()});
+        this.reset.addEventListener('click', () => {this.onReset()});
+        // this.display.addEventListener('focus', (event) => {this.onInput(event)});
+        this.display.addEventListener('input', (event) => {this.onInput(event)});
+    }
+
+    onStart() {
+        console.log('Start Timer');
+        this.setTime();
+        this.startTimer();
+        this.setControls(true, false); 
+        this.display.classList.add('progress');
+        this.setAllInputsDisabled(true);
+    }
+
+    setTime() {
+       this.minutes = parseInt(this.minMSB.value)*10 + parseInt(this.minLSB.value);
+       this.seconds = parseInt(this.secMSB.value)*10 + parseInt(this.secLSB.value);
+    }
+
+    startTimer() {
+        if(this.minutes === 0 && this.seconds === 0) {
+            this.onReset();
+            return;
+        }
+
+        this.intervalId = setInterval(()=> {
+            this.seconds-=1;
+
+            if(this.seconds < 0) {
+                this.seconds = 59;
+                this.minutes-=1;
+            }
+
+            if(this.minutes === 0 && this.seconds === 0) {
+                this.onReset();
+                // TODO: there is a known issue -> fix the start button activated and stop button deactivated
+            }
+
+            this.setDisplay(this.minutes, this.seconds);
+        }, 1000);
+    }
+
+    setDisplay(mins, secs) {
+        this.minMSB.value = String(Math.floor(mins/10));
+        this.minLSB.value = String(mins%10);
+        this.secMSB.value = String(Math.floor(secs/10));
+        this.secLSB.value = String(secs%10);
+    }
+
+    onStop() {
+        clearInterval(this.intervalId);
+        this.setControls(false, true); 
+        this.display.classList.remove('progress'); 
+        this.setAllInputsDisabled(false);
+    }
+
+    setControls(startStatus=false, stopStatus=false) {
+        this.start.disabled = startStatus;
+        this.stop.disabled = stopStatus;
+    }
+
+    setAllInputsDisabled(isDisabled=false) {
+        this.minMSB.disabled = isDisabled;
+        this.minLSB.disabled = isDisabled;
+
+        this.secMSB.disabled = isDisabled;
+        this.secLSB.disabled = isDisabled;
+    }
+   
+    onReset() {
+       console.log('Reset button');
+       // resetting mt timer as 00:00
+       this.resetTimerValues();
+       this.resetControls();
+    }
+
+    resetControls() {
+        clearInterval(this.intervalId);
+        this.setAllInputsDisabled(false);
+        this.display.classList.remove('progress');
+    }
+
+    resetTimerValues() {
+        this.minMSB.value = 0;
+        this.minLSB.value = 0;
+        this.secMSB.value = 0;
+        this.secLSB.value = 0;
+    }
+
+    onInput(event) {
+        //minMSB & secMSB -> 0 - 5
+        // minLSB & secLSB -> 0 - 9
+
+        // const value = Number(event.target.value);
+        const value = Number(event.data);
+        if(Number.isInteger(value)) {
+            console.log(document.activeElement);
+            console.log(value);
+            if (this.minMSB === document.activeElement){
+                this.onValue(this.minMSB, this.minLSB, value, 0, 5);
+            } else if (this.minLSB === document.activeElement) {
+                this.onValue(this.minLSB, this.secMSB, value, 0, 9);
+            } else if(this.secMSB === document.activeElement){
+                this.onValue(this.secMSB, this.secLSB, value, 0, 5);
+            } else if(this.secLSB === document.activeElement){
+                this.onValue(this.secLSB, null, 0, 9);
+            }
+        }
+
+    }
+
+    onValue(target, nextTarget, value, minAllowed, maxAllowed) {
+        if(value >= minAllowed && value <= maxAllowed) {
+            target.value = value;
+            if(nextTarget) {
+                nextTarget.focus();
+                nextTarget.select();
+            } else {
+                target.value = 0;
+                target.select();
+            }
+        } else {
+            target.value = 0;
+        }
+    }
+
+    // errorMsg() {
+
+    // }
+
+}
+
+new Timer('minMSB', 'minLSB', 'secMSB', 'secLSB', 'start', 'stop', 'reset', 'display');
+
+
