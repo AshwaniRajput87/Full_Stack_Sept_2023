30a2f0289dfeb306cc405b6cececb7a5e31bd4f6
Ashwani Rajput <ashwin.rajput87@gmail.com>
2023-11-08 11:56:36 +0530

    LEc-4 - React router , redirects, private routes and not found pages and Ecoomerce project has been setup and started

---
+++ React/Lec-4_React_Router_DOM_Ecommerce_Project/ecomm-app/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../navbar/Navbar";
+import './header.css';
+
+const Header = () => {
+    return (
+        <>
+          <header className="header">
+            <Navbar />
+          </header> 
+        </>
+    )
+}
+
+export default Header;
---
+++ React/Lec-4_React_Router_DOM_Ecommerce_Project/ecomm-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Link, NavLink }  from 'react-router-dom';
+
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='nav'>
+            <div className='nav-left'>
+                <ul className='nav-items'>
+                    <li className="nav-item">
+                        <NavLink 
+                            to='/'
+                            className="nav-link">
+                                Electronics
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink
+                            to='/about'
+                            className="nav-link">Jwelery
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink 
+                            to='/contact'
+                            className="nav-link">Men's Clothing
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink 
+                            to='/contact'
+                            className="nav-link">Women's Clothing
+                        </NavLink>
+                    </li>
+
+                </ul>
+            </div>
+            <div className='nav-right'>
+                <Link to="/cart" className="cart-icon-container">
+                    cart
+                </Link>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar;
