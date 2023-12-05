c0aee0b54ea887f9a9c406c51507d53d4985e4fd
naveenkumar.kothakapu <naveenkumar.kothakapu@jocata.com>
2023-12-05 19:16:14 +0530

    e-commerce project build file

---
+++ React/React-Ecommerce-Project/ecomm-app/dist/assets/Loader-21d29c6b.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-b2b99665.js";const r=()=>a.jsx("div",{className:"loader-overlay",children:a.jsx("div",{className:"loader"})});export{r as default};
---
+++ React/React-Ecommerce-Project/ecomm-app/dist/assets/useCart-7d2e1af4.js
@@ -0,0 +1,1 @@
+import{r as t,C as r}from"./index-b2b99665.js";const o=()=>t.useContext(r);export{o as u};
